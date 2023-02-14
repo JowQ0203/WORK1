737af9667b5726e327bb193b41fdac92150d3595
JowQ0203 <125360789+JowQ0203@users.noreply.github.com>
2023-02-14 19:14:07 +0700

    add insert.php

---
+++ insret.php
@@ -0,0 +1,18 @@
+<?php 
+require 'connectdb';
+
+$xxx  = $_POST[''];
+$xxx  = $_POST[''];
+$xxx  = $_POST[''];
+$xxx  = $_POST[''];
+$xxx  = $_POST[''];
+
+$query = "INSERT INTO xxx() VALUES()";
+$result = mysqli_query($connectdb,$query);
+
+if($query){
+echo 'ผ่าน';
+}else{
+echo 'ไม่ผ่าน';
+}
+?>
